6827a980011a46633eac0627f26aaad0e0d06700
alpha artimos <remonansary@gmail.com>
2021-11-25 10:24:52 +0600

    first commit

---
+++ src/components/shop/home/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Test() {
+  return (
+    <div>
+      hello world
+      <div></div>
+    </div>
+  );
+}
+
+export default Test;
---
+++ src/components/shop/index.js
@@ -0,0 +1,31 @@
+import Home from "./home";
+import Shop from "./Shop";
+import WishList from "./wishlist";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import AdminProtectedRoute from "./auth/AdminProtectedRoute";
+import CartProtectedRoute from "./auth/CartProtectedRoute";
+import { LayoutContext } from "./layout";
+import { layoutState, layoutReducer } from "./layout/layoutContext";
+import { isAdmin, isAuthenticate } from "./auth/fetchApi";
+import PageNotFound from "./layout/PageNotFound";
+import ProductDetails from "./productDetails";
+import ProductByCategory from "./home/ProductByCategory";
+import CheckoutPage from "./order/CheckoutPage";
+
+export {
+  Home,
+  Shop,
+  WishList,
+  ProtectedRoute,
+  AdminProtectedRoute,
+  CartProtectedRoute,
+  LayoutContext,
+  layoutState,
+  layoutReducer,
+  isAdmin,
+  isAuthenticate,
+  PageNotFound,
+  ProductDetails,
+  ProductByCategory,
+  CheckoutPage,
+};
